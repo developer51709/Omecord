7a04dc0b24a3c368a8ac41c7caee1949654e7d8e
𝐍𝐲𝐱𝐞𝐧 <jgarland13191@gmail.com>
2026-02-15 15:26:33 +0000

    Update core/banner.js

--- src/core/banner.js
+++ src/core/banner.js
@@ -13,5 +13,9 @@ export function printBanner() {
     );
 
     console.log(chalk.cyan("           Discord‑Native Random Video Platform"));
-    console.log(chalk.gray("----------------------------------------------------------------"));
+    console.log(chalk.gray("─────────────────────────────────────────────────────────────"));
+    console.log(chalk.gray("→ Pre-Alpha Version (this version is not stable and may break at any time)"));
+    console.log(chalk.gray("→ Made with ♡ by Nyxen"));
+    console.log(chalk.gray("→ https://github.com/developer51709/Omecord"));
+    console.log(chalk.gray("─────────────────────────────────────────────────────────────"));
 }
