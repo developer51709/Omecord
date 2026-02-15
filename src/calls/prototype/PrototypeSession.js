48552bc18f953bbb4fe5d28011b1a51399fdfb94
𝐍𝐲𝐱𝐞𝐧 <jgarland13191@gmail.com>
2026-02-15 01:05:21 +0000

    Added test mode to call prototype

--- src/calls/prototype/PrototypeSession.js
+++ src/calls/prototype/PrototypeSession.js
@@ -2,21 +2,20 @@
 
 import {
     joinVoiceChannel,
-    getVoiceConnection,
-    EndBehaviorType,
-    createAudioPlayer,
-    createAudioResource
+    getVoiceConnection
 } from "@discordjs/voice";
 
-import { PassThrough } from "stream";
 import { PrototypeAudioReceiver } from "./PrototypeAudioReceiver.js";
 import { PrototypeAudioMixer } from "./PrototypeAudioMixer.js";
 import { PrototypeAudioSender } from "./PrototypeAudioSender.js";
 
 export class PrototypeSession {
-    constructor(vcA, vcB) {
+    constructor(vcA, vcB, options = {}) {
         this.vcA = vcA;
         this.vcB = vcB;
+
+        this.testMode = options.testMode || false;
+        this.fakeConnectionFactory = options.fakeConnectionFactory || null;
 
         this.receiverA = null;
         this.receiverB = null;
@@ -27,8 +26,7 @@ export class PrototypeSession {
         this.mixer = new PrototypeAudioMixer();
     }
 
-    async start() {
-        // Connect to both voice channels
+    _connectReal() {
         const connA = joinVoiceChannel({
             channelId: this.vcA.id,
             guildId: this.vcA.guild.id,
@@ -40,6 +38,25 @@ export class PrototypeSession {
             guildId: this.vcB.guild.id,
             adapterCreator: this.vcB.guild.voiceAdapterCreator
         });
+
+        return { connA, connB };
+    }
+
+    _connectFake() {
+        if (!this.fakeConnectionFactory) {
+            throw new Error("Test mode enabled but no fakeConnectionFactory provided.");
+        }
+
+        return this.fakeConnectionFactory();
+    }
+
+    async start() {
+        const { connA, connB } = this.testMode
+            ? this._connectFake()
+            : this._connectReal();
+
+        this.connA = connA;
+        this.connB = connB;
 
         // Create receivers
         this.receiverA = new PrototypeAudioReceiver(connA, "A");
@@ -62,12 +79,15 @@ export class PrototypeSession {
             this.senderB.send(mixed);
         });
 
-        console.log("Prototype call session started.");
+        console.log(`Prototype call session started in ${this.testMode ? "TEST" : "REAL"} mode.`);
     }
 
     stop() {
-        getVoiceConnection(this.vcA.guild.id)?.destroy();
-        getVoiceConnection(this.vcB.guild.id)?.destroy();
+        if (!this.testMode) {
+            getVoiceConnection(this.vcA.guild.id)?.destroy();
+            getVoiceConnection(this.vcB.guild.id)?.destroy();
+        }
+
         console.log("Prototype call session stopped.");
     }
 }
